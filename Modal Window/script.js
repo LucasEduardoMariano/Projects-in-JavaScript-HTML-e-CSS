ee55aebce799487e7f861e92025404be59ae1b24
Lucas Mariano <lucasemariano29@outlook.com>
2024-11-06 14:37:17 +0000

    commit initial

---
+++ Modal Window/script.js
@@ -0,0 +1,11 @@
+function openModal(){
+    const modal = document.getElementById('modal-window')
+    modal.classList.add('open')
+
+    modal.addEventListener('click', (e) =>{
+        if(e.target.id == 'close' || e.target.id == 'modal-window'){
+            modal.classList.remove('open')
+        }
+
+    })
+}
